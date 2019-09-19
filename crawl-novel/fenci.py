c3b4465968c871f473a78d29abdc0d413d38abab
liuyuzhangolvz <34113025+liuyuzhangolvz@users.noreply.github.com>
2019-09-19 20:14:25 +0800

    Update fenci.py

--- crawl-novel/fenci.py
+++ crawl-novel/fenci.py
@@ -8,26 +8,19 @@ from pymongo import MongoClient
 # 定义 mongodb 连接对象
 mongo = MongoClient()
 # 使用的 mongodb 指定数据库数据表
-db = mongo['yz']['xiaoshuo']
+db = mongo['jinyong']['xiaoshuo']
+save_path = "F:/jinyong/data/persons.txt"
 
-''' 结果保存地址
-
-保存格式
-
-word \t tag
-'''
-save_path = "word_tag.txt"
-
-print("processing...")
-pairs = []
+print("start processing...")
+persons = []
 for book_obj in db.find():
     for chapter in book_obj["chapters"]:
         for word, tag in pseg.cut(chapter['content']):
-            pairs.append((word, tag))
-
+            if tag == "nr":
+              persons.append(word)
 print("save to {}...".format(save_path))
 # 去重
-pairs_set = set(pairs)
+persons = list(set(persons))
 with open(save_path, "w") as wf:
-    for word, tag in pairs_set:
-        wf.writelines("{}\t{}\n".format(word, tag))
+    for word in persons:
+        wf.writelines("{}\n".format(word))
