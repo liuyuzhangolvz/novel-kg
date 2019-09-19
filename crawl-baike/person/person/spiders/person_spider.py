7edc5b8d6ccedf603bd7dec013ae6d1f73286dcc
liuyuzhangolvz <34113025+liuyuzhangolvz@users.noreply.github.com>
2019-09-19 20:37:13 +0800

    Update person_spider.py

--- crawl-baike/person/person/spiders/person_spider.py
+++ crawl-baike/person/person/spiders/person_spider.py
@@ -8,11 +8,20 @@ import scrapy
 from pymongo import MongoClient
 
 # 常量
-PERSONS_FILE = 'D:/毕业设计/crawl/persons.txt' # 人物列表存放地址，这里最好用绝对地址哦
+PERSONS_FILE = 'F:/jinyong/data/persons.txt' # 人物列表存放地址，这里最好用绝对地址哦
+DB_NAME = 'jinyong'   # mongodb 库名
+TABLE_NAME = 'persons'  # mongodb 表名
+DROP_KEYS = ['中文名', '饰演', '配音']  # 要删除的 info 键名
+KEYWORDS = ['金庸', '飞狐外传', '雪山飞狐', '连城诀', '天龙八部', '射雕英雄传',
+        '白马啸西风', '鹿鼎记', '笑傲江湖', '书剑恩仇录', '神雕侠侣',
+        '侠客行', '倚天屠龙记', '碧血剑', '鸳鸯刀', '越女剑']
+
 # 变量
 mongo = MongoClient()  # mongodb 的操作对象，使用默认参数即可
-db = mongo["yz"]["persons"]  # yz 是 mongo 的数据库， persons 是库 yz 下的一张表
+mongo[DB_NAME].drop_collection(TABLE_NAME)  # 删除 persons 数据库，好重写
+db = mongo[DB_NAME][TABLE_NAME]  # yz 是 mongo 的数据库， persons 是库 yz 下的一张表
 re_split = re.compile(r'[,、，；]')  # 字符串切割正则
+re_match = re.compile(r'({})'.format('|'.join(KEYWORDS)))  # 生成关键词正则，表示网页中必须最少含有这些关键词的一个
 
 def strQ2B(content):
     """全角转半角"""
@@ -47,7 +56,7 @@ def get_urls():
     """从人物列表中获取url"""
     url_format = 'https://baike.baidu.com/item/{}'
     urls = []
-    with open(PERSONS_FILE, 'r', encoding="utf-8") as rf:
+    with open(PERSONS_FILE, 'r', encoding='utf-8') as rf:
         for line in rf:
             urls.append(url_format.format(line.strip()))
     return urls
@@ -57,14 +66,14 @@ class PersonSpider(scrapy.Spider):
     name = 'person_spider' # 爬虫名称，和文件名一致
     allowed_domins = ['baike.baidu.com'] # 允许爬取的域名
     start_urls = get_urls() # 要爬去的 url
-    fail_handle = open('./fail.txt', 'w') # 失败的文件资源，对于插入失败的人物可能是百科中出现了歧义，所以需要人工检查插入
+    fail_handle = open('./fail.txt', 'w', encoding='utf-8') # 失败的文件资源，对于插入失败的人物可能是百科中出现了歧义，所以需要人工检查插入
 
     def parse(self, response):
         """ 解析页面，这里只存储 basic-info 的内容，且要判断 '金庸' 二字是否出现在页面中
         """
         # 通过 xpath 语法获取当前人物名
         person = response.xpath("//dd[@class='lemmaWgt-lemmaTitle-title']//h1/text()").extract_first()
-        if '金庸' in clean_content(response.xpath("//div[@class='content-wrapper']").extract_first()):
+        if re_match.findall(clean_content(response.xpath("//div[@class='content-wrapper']").extract_first())):
             # names 代表 basic-info 里的键名
             names = response.xpath("//div[contains(concat(' ', normalize-space(@class), ' '), 'basic-info')]//dt[contains(concat(' ', normalize-space(@class), ' '), 'name')]").extract()
             # vals 代表 baisic-info 里的值
@@ -86,10 +95,12 @@ class PersonSpider(scrapy.Spider):
                 # 失败则插入文件
                 self.fail_handle.writelines("{} {}\n".format(str(person), response._url))
             else:
-                # 插入 mongodb
                 info = dict(zip(names, values))
-                del info["中文名"]
-                db.save({"person": person, "info": info})#zip把names，values两个列表转换成键值对
+                for drop_key in DROP_KEYS:
+                    if drop_key in info:
+                        del info[drop_key]
+                # 插入 mongodb
+                db.save({"person": person, "info": info})
         else:
             if person is not None:
                 # 失败则插入文件
