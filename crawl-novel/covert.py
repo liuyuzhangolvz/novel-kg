0a6b9f288de8ba79bc63f66c7fd4daec6c59c5d3
liuyuzhangolvz <34113025+liuyuzhangolvz@users.noreply.github.com>
2019-10-23 11:37:41 +0800

    Update covert.py

--- crawl-novel/covert.py
+++ crawl-novel/covert.py
@@ -10,10 +10,10 @@ dirname = "F:/jinyong/data/books"
 for book_obj in db.find():
     print("start to process {}...".format(book_obj["book_name"]))
 
-    book_dir = os.path.join(dirname, book_obj["book_name"]+".txt")
+    book_dir = os.path.join(dirname, book_obj["book_name"])
     os.makedirs(book_dir, exist_ok=True)
 
     for chapter in book_obj["chapters"]:
-        chapter_fname = os.path.join(book_dir, chapter['name'])
+        chapter_fname = os.path.join(book_dir, chapter['name']+".txt")
         with open(chapter_fname, "w", encoding="utf-8") as wf:
             wf.writelines(chapter['content'] + "\n")
